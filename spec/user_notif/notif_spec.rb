78d49b9f785a26cb95ce90d8db2bf6e2750698a3
Thibaut Roche <thibaut.roche.perso@gmail.com>
2016-04-25 18:00:54 +0200

    [C] Notif spec

--- spec/user_notif/notif_spec.rb
+++ spec/user_notif/notif_spec.rb
@@ -15,7 +15,27 @@ ActiveRecord::Base.connection.instance_eval do
 end
 
 describe UserNotif::Notif do
+  let(:notif) { UserNotif::Notif.new }
+
   it 'raises an exception with an invalid target' do
     expect { UserNotif::Notif.new(target: nil) }.to raise_error ModelExceptions::BadTypeNotification
+  end
+
+  describe '#email?' do
+    it 'returns true' do
+      expect(notif.email?).to eq true
+    end
+  end
+
+  describe '#email_template' do
+    it 'returns generic_notif' do
+      expect(notif.email_template).to eq 'generic_notif'
+    end
+  end
+
+  describe '#subject_email' do
+    it 'returns subject_email' do
+      expect(notif.subject_email).to eq I18n.t('notif.generic.subject')
+    end
   end
 end
